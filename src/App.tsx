f562459acfaa340638d917304aece1cb6ff5dc9c
Dominic Prior <dominicprior42@gmail.com>
2025-08-23 14:05:13 +0100

    WIP - not maintaining state across page refreshes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-// import { useState, useEffect, } from 'react';
+import { useState, useEffect, } from 'react';
 
 let n = 0;
 
 export default function App() {
-    // const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
     console.log('hi from App');
 
-    // useEffect(() => {
-    //     const savedCount = 5;  // +(localStorage.getItem('count') ?? 7);
-    //     n++;
-    //     console.log('getting ' + savedCount + ' : ' + n);
-    //     // setCount(savedCount);
-    // },
-    // [ ]);
+    useEffect(() => {
+        const savedCount = +(localStorage.getItem('count') ?? 7);
+        n++;
+        console.log('getting ' + savedCount + ' : ' + n);
+        setCount(savedCount);
+    },
+    [ ]);
 
-    // useEffect(() => {
-    //     localStorage.setItem('count', '' + count)
-    //     n++;
-    //     console.log('setting to ' + count + ' : ' + n);
-    // },
-    // [ count ]);
+    useEffect(() => {
+        localStorage.setItem('count', '' + count)
+        n++;
+        console.log('setting to ' + count + ' : ' + n);
+    },
+    [ count ]);
 
-    // function buttonClick() {
-    //     setCount(count + 1);
-    // }
+    function buttonClick() {
+        setCount(count + 1);
+    }
 
     return (
         <div>
-            {/* <div>count: { count }</div> */}
-            {/* <button onClick={ buttonClick }>click me</button> */}
+            <div>count: { count }</div>
+            <button onClick={ buttonClick }>click me</button>
         </div>
     );
 }
